dfba92a905dd52814dff90c36db73c69eb914f94
Pedro Henrique <doctorfast10.12@hotmail.com>
2025-01-16 22:12:11 -0300

    chore: location

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Box about='Location' sx={{height: "80vh"}}>
         <Typography style={{fontSize: "2rem", color: "#80533f", fontWeight: "900" }}>Onde estamos?</Typography>
         <Box>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1258.8448910549985!2d-45.93925233060304!3d-23.2837127918973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cdcadee358c46f%3A0x796611ce3e87f383!2sPadaria%20arco%20iris!5e0!3m2!1spt-BR!2sbr!4v1736809091875!5m2!1spt-BR!2sbr" width="600" height="450" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3664.89552555399!2d-45.941511324678316!3d-23.283245578991785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cdcbd607494e8f%3A0x5a009d346a923794!2sCalifornia%20Informatica!5e0!3m2!1spt-BR!2sbr!4v1737076259578!5m2!1spt-BR!2sbr" width="600" height="450" loading="lazy"></iframe>
         </Box>
       </Box>
       <Contact />
