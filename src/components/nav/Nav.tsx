b3dd103b34d57dc579069830aafdc530ff8786bf
Pedro Henrique Admin <pedrohenrique.admin@capricho.local>
2025-02-04 14:35:10 -0300

    chore public

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -7,7 +7,7 @@ export const Nav = () => {
     <>
     <AppBar style={{ background: "#191919", position: "sticky", height: "85px", boxShadow: "none", display: "flex", justifyContent: "flex-end", borderBottom: "1px solid #232323"}}>
       <Toolbar sx={{ display: "flex", gap: "15px", ml: 6}}>
-        <img src="public/assets/images/GrizzlyBear.png" style={{ height: "90px" }} />
+        <img src="/assets/images/GrizzlyBear.png" style={{ height: "90px" }} />
         <Box >
           <Typography style={{ fontFamily: 'Bernier', fontSize: "2rem", color: "#EEEEEE", fontWeight: "300" }}>
             California Informatica
