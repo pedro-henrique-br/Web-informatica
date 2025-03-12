5a39b2692950c3ccfefd9b8b128c16dcbdf2675e
Pedro Henrique <71238431+pedro-henrique-br@users.noreply.github.com>
2025-03-11 21:25:31 -0300

    Update About.tsx

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -24,7 +24,7 @@ export const About = () => {
         <Typography sx={{fontFamily: "Montserrat", color: "#EEEEEE", fontSize: isMobile ? "1.7rem" : "2rem", fontWeight: "600"}}>Informatica California Soluções em TI</Typography>
         <Typography sx={{fontFamily: "Montserrat", color: "#EEEEEE", fontSize: isMobile ? "1.3rem" : "1.7rem"}}>Assistência Técnica de Informática</Typography>
       <Box>
-        <img style={{width: isMobile ? "300px" : "400px"}} src="https://images.tcdn.com.br/img/img_prod/827530/placa_de_video_geforce_rtx_3050_8gb_gigabyte_windforce_oc_edition_999_1_f0efbce236e8d6da952acf00484b984c.png" />
+        <img style={{width: isMobile ? "300px" : "400px"}} src="/assets/images/pc.png" />
       </Box>
       </Box>
     </Box>
